368538de14d2bd454dde4a8eb449f526cbf0a415
Efimchuk Andrey <aefimchuk@ozon.ru>
2023-04-22 23:10:09 +0300

    refactor relations

--- internal/domain/relation.go
+++ internal/domain/relation.go
@@ -6,28 +6,28 @@ import (
 )
 
 type (
-	RelationType  string
-	OnActionType  string
-	OperationType string
+	RelationType string
+	//OnActionType  string
+	//OperationType string
 )
 
 const (
-	OneToOne        RelationType  = "one_to_one"
-	OneToMany       RelationType  = "one_to_many"
-	ManyToMany      RelationType  = "many_to_many"
-	Cascade         OnActionType  = "cascade"
-	Restrict        OnActionType  = "restrict"
-	SetNull         OnActionType  = "set null"
-	SetDefault      OnActionType  = "set default"
-	NoAction        OnActionType  = "no action"
-	DeleteOperation OperationType = "delete"
-	UpdateOperation OperationType = "update"
+	OneToOne   RelationType = "one_to_one"
+	OneToMany  RelationType = "one_to_many"
+	ManyToMany RelationType = "many_to_many"
+	//Cascade         OnActionType  = "cascade"
+	//Restrict        OnActionType  = "restrict"
+	//SetNull         OnActionType  = "set null"
+	//SetDefault      OnActionType  = "set default"
+	//NoAction        OnActionType  = "no action"
+	//DeleteOperation OperationType = "delete"
+	//UpdateOperation OperationType = "update"
 )
 
 type OnDeleteUpdate struct {
-	Operation OperationType `json:"operation"`
-	Action    OnActionType  `json:"action"`
-	Column    string        `json:"column,omitempty"`
+	Operation string   `json:"operation"`
+	Action    string   `json:"action"`
+	Columns   []string `json:"columns,omitempty"`
 }
 
 type RelationTable struct {
@@ -37,10 +37,10 @@ type RelationTable struct {
 }
 
 type Relation struct {
-	ReferenceTable RelationTable   `json:"reference_table"` // кто ссылается
-	ReferenceTo    RelationTable   `json:"reference_to"`    // куда ссылается
-	RelationType   RelationType    `json:"relation_type"`
-	OnDeleteUpdate *OnDeleteUpdate `json:"on_delete,omitempty"`
+	ReferenceTable RelationTable    `json:"reference_table"` // кто ссылается
+	ReferenceTo    RelationTable    `json:"reference_to"`    // куда ссылается
+	RelationType   RelationType     `json:"relation_type"`
+	OnDeleteUpdate []OnDeleteUpdate `json:"on_delete,omitempty"`
 }
 
 func (r *Relation) BuildRelationSql(builder *strings.Builder) {
@@ -55,47 +55,55 @@ func (r *Relation) BuildRelationSql(builder *strings.Builder) {
 }
 
 func (r *Relation) buildOneToOne(builder *strings.Builder) {
+	var onAction string
+	if r.OnDeleteUpdate != nil {
+		onAction = fmt.Sprintf(` on %s %s`, r.OnDeleteUpdate[0].Operation, r.OnDeleteUpdate[0].Action)
+	}
 	str := fmt.Sprintf(`
 alter table %s.%s
-    add constraint fk_%s_%s foreign key (%s) references %s.%s (%s);
+    add constraint fk_%s_%s foreign key (%s) references %s.%s (%s) %s;
 `,
 		r.ReferenceTable.Schema, r.ReferenceTable.Table, r.ReferenceTable.Table, r.ReferenceTo.Table, r.ReferenceTable.Field,
-		r.ReferenceTo.Schema, r.ReferenceTo.Table, r.ReferenceTo.Field)
-	if r.OnDeleteUpdate != nil {
-		str = strings.TrimSuffix(str, `;
-`)
-		switch r.OnDeleteUpdate.Action {
-		case SetNull, SetDefault:
-			str += fmt.Sprintf(` on %s %s (%s);
-`, r.OnDeleteUpdate.Operation, r.OnDeleteUpdate.Action, r.OnDeleteUpdate.Column)
-		default:
-			str += fmt.Sprintf(` on %s %s;
-`, r.OnDeleteUpdate.Operation, r.OnDeleteUpdate.Action)
-		}
-	}
+		r.ReferenceTo.Schema, r.ReferenceTo.Table, r.ReferenceTo.Field, onAction)
+
 	builder.WriteString(str)
 }
 
 func (r *Relation) buildOneToMany(builder *strings.Builder) {
+	var onAction string
+	if r.OnDeleteUpdate != nil {
+		onAction = fmt.Sprintf(` on %s %s`, r.OnDeleteUpdate[0].Operation, r.OnDeleteUpdate[0].Action)
+	}
 	str := fmt.Sprintf(`
 alter table %s.%s
-    add constraint fk_%s_%s foreign key (%s) references %s.%s (%s);
+    add constraint fk_%s_%s foreign key (%s) references %s.%s (%s) %s;
 `,
 		r.ReferenceTable.Schema, r.ReferenceTable.Table, r.ReferenceTable.Table, r.ReferenceTo.Table, r.ReferenceTable.Field,
-		r.ReferenceTo.Schema, r.ReferenceTo.Table, r.ReferenceTo.Field)
+		r.ReferenceTo.Schema, r.ReferenceTo.Table, r.ReferenceTo.Field, onAction)
+
 	builder.WriteString(str)
 }
 
 func (r *Relation) buildManyToMany(builder *strings.Builder) {
+	var (
+		onActionReferenceTable string
+		onActionReferenceTo    string
+	)
+	if r.OnDeleteUpdate != nil {
+		onActionReferenceTable = fmt.Sprintf(` on %s %s`, r.OnDeleteUpdate[0].Operation, r.OnDeleteUpdate[0].Action)
+		onActionReferenceTo = fmt.Sprintf(` on %s %s`, r.OnDeleteUpdate[1].Operation, r.OnDeleteUpdate[1].Action)
+	}
+
 	str := fmt.Sprintf(`
 create table %s.%s_%s
 (
-    "%s"    int references %s.%s (%s),
-    "%s"    int references %s.%s (%s)
+    "%s"    int references %s.%s (%s) %s,
+    "%s"    int references %s.%s (%s) %s
 );
 `,
 		r.ReferenceTable.Schema, r.ReferenceTable.Table, r.ReferenceTo.Table,
-		r.ReferenceTable.Field, r.ReferenceTable.Schema, r.ReferenceTable.Table, r.ReferenceTable.Field,
-		r.ReferenceTo.Field, r.ReferenceTo.Schema, r.ReferenceTo.Table, r.ReferenceTo.Field)
+		r.ReferenceTable.Field, r.ReferenceTable.Schema, r.ReferenceTable.Table, r.ReferenceTable.Field, onActionReferenceTable,
+		r.ReferenceTo.Field, r.ReferenceTo.Schema, r.ReferenceTo.Table, r.ReferenceTo.Field, onActionReferenceTo)
+
 	builder.WriteString(str)
 }
